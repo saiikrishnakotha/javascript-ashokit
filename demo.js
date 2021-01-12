288949e75f1e64e4dc0eddd9eb7539552384643e
saikrishna <sk.saikrishnakotha@gmail.com>
2021-01-12 23:31:55 +0530

    introduction

---
+++ demo.js
@@ -0,0 +1,25 @@
+console.log("welcome to javascript");
+
+console.log (100 == "100")  // true
+console.log (100 === "100")  // false
+console.log( 10 =="ten")  // false
+console.log(10 ==="ten")  // false
+
+
+// operators 
+
+console.log( 10 + "10")  //1010
+console.log(10-"10") // 0
+console.log(10 * "10") //100
+console.log(10/"10")  //1
+console.log(10 + + "10" ) // 20
+
+
+console.log(0.1+0.2 == 0.3) // false
+console.log(0.1+0.2 === 0.3) //false
+console.log(0.1+0.2) //  0.30000000000000004
+
+console.log(undefined == undefined)  // true 
+console.log(undefined == null)       // true
+console.log(undefined === undefined)  // true
+console.log(undefined === null)       //false
